212f9ce8ab02a17d1ab8b5d5f142a8e73ee63f7a
anastasia <anastasia.findyourdog@gmail.com>
2019-02-28 08:50:17 +0300

    Task 3 code upd

--- task_3.py
+++ task_3.py
@@ -13,13 +13,11 @@ numbers = [random.randint(MIN_LIMIT, MAX_LIMIT) for _ in range(SIZE)]
 
 max_id = 0
 min_id = 0
-for i in range(1, SIZE):
+for i in range(1, SIZE): # начинать с 0 элемента не нужно, тк это значение присвоено по умолчанию
     if numbers[i] > numbers[max_id]:
         max_id = i
-        print(f'new max {numbers[i]}, {max_id}')
     if numbers[i] < numbers[min_id]:
         min_id = i
-        print(f'new min {numbers[i]}, {min_id}')
 
 print(f'Original array: {numbers}')
 numbers[min_id], numbers[max_id] = numbers[max_id], numbers[min_id]
