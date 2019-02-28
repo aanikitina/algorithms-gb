bb66d01e7bd86846c2e5d58b2dd4a6fcefe890b6
anastasia <anastasia.findyourdog@gmail.com>
2019-02-28 07:39:57 +0300

    Task 1 code updated

--- task_1.py
+++ task_1.py
@@ -3,8 +3,6 @@ Task 1:
 
 В диапазоне натуральных чисел от 2 до 99 определить, сколько из них кратны любому из чисел в диапазоне от 2 до 9.
 """
-import random
-
 N_MAX_LIMIT = 99
 N_MIN_LIMIT = 2
 D_MAX_LIMIT = 9
@@ -12,13 +10,11 @@ D_MIN_LIMIT = 2
 
 
 numbers = [i for i in range(N_MIN_LIMIT, N_MAX_LIMIT + 1)]
-divider = random.randint(D_MIN_LIMIT, D_MAX_LIMIT + 1)
-print(f'Divider - {divider}')
 
-result_list = []
+for divider in range(D_MIN_LIMIT, D_MAX_LIMIT + 1):
+    result_list = []
+    for number in numbers:
+        if (number % divider) == 0:
+            result_list.append(number)
 
-for number in numbers:
-    if (number % divider) == 0:
-        result_list.append(number)
-
-print(len(result_list))
+    print(f'{divider} - {len(result_list)}')
